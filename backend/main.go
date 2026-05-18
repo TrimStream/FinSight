e8abb169f767045f799cc571cd1a58ab38844727
Eshaan Singh <86927949+TrimStream@users.noreply.github.com>
2026-05-18 16:11:30 -0700

    Simplify .env loading and update connection string

--- backend/main.go
+++ backend/main.go
@@ -38,20 +38,18 @@ type CompanyOverview struct {
 var db *sql.DB
 
 func main() {
-	// Load .env file
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// Load .env file if present
+	godotenv.Load()
 
 	// Connect to PostgreSQL
 	connStr := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_PORT"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME"),
+		os.Getenv("DB_SSLMODE"),
 	)
 
 	db, err = sql.Open("postgres", connStr)
